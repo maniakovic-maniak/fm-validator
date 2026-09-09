17afc6a0b16ae04314d5e58e95e78183b8012cd9
Mikhail Riazanov <maniakovic@gmail.com>
2026-09-10 05:50:20 +1000

    Test bug-scan agent after fixing key and credits

--- src/validator-tier2.js
+++ src/validator-tier2.js
@@ -902,3 +902,4 @@ async function runTier2(parsed, { domain = '', domainFile = '', modelContext = '
 }
 
 module.exports = { runTier2, parseResponse, resolveDeepAccountingSheets, extractMeaningfulRows };
+// workspace-id fix test Thu Sep 10 05:50:20 AEST 2026
