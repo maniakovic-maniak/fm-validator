ea8014d176e7fef287e8db3d78e11913be8c1a37
Mikhail Riazanov <maniakovic@gmail.com>
2026-09-09 18:14:53 +1000

    Test bug-scan agent with a real file change after workspace-id fix

--- src/validator-tier2.js
+++ src/validator-tier2.js
@@ -902,3 +902,4 @@ async function runTier2(parsed, { domain = '', domainFile = '', modelContext = '
 }
 
 module.exports = { runTier2, parseResponse, resolveDeepAccountingSheets, extractMeaningfulRows };
+// workspace-id fix test Wed  9 Sep 2026 18:14:53 AEST
